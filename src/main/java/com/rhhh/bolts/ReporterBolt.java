c3a9c6d4bec9d29a3730b85e4d16cfed928c36e5
Nir Shalmon <shalmonir@gmail.com>
2017-09-27 09:34:02 +0300

    Fixed Tests and flow issues.

--- src/main/java/com/rhhh/bolts/ReporterBolt.java
+++ src/main/java/com/rhhh/bolts/ReporterBolt.java
@@ -10,6 +10,7 @@ import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.tuple.Tuple;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.*;
 import java.util.*;
 
@@ -23,10 +24,20 @@ public class ReporterBolt implements IRichBolt {
     private int epsilon = 1000;
     private double theta = 0.001;
     private long N = 0;
+    private PrintWriter writer;
+    private StreamSummary<String>[] current_counters = null;
 
     @Override
     public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
         this.collector = outputCollector;
+        current_counters = new StreamSummary[5];
+        try{
+            writer = new PrintWriter("Report.txt", "UTF-8");
+            writer.println("Reporter report Started\n");
+//            writer.close();
+        } catch (IOException e) {
+            // do something
+        }
     }
 
     @Override
@@ -34,10 +45,12 @@ public class ReporterBolt implements IRichBolt {
         try {
             Connection conn = DriverManager.getConnection(DBUtils.RHHH_URL, DBUtils.USER, DBUtils.PASS);
             stmt = conn.createStatement();
-            N = getTotalPacketNumber();
+//            N = getTotalPacketNumber();
             //String sqlSelectTotalLevelCounter = "SELECT LAST 1 total FROM Level";
             Map<String,Long> hhhmap = getHeavyHitters(1);
-            System.out.println("@@"+hhhmap);
+//            System.out.println("@@"+hhhmap);
+            if(hhhmap != null)
+                writer.println("Total = " + N + ". HH list: " + hhhmap + "\n");
         }
         catch (SQLException e){
             e.printStackTrace();
@@ -49,8 +62,13 @@ public class ReporterBolt implements IRichBolt {
     }
 
     private Map<String,Long> getHeavyHitters(int level) throws SQLException, IOException, ClassNotFoundException {
+        getStreamSummaryForLevelAndUpdateTotal();
+        for (int i = 1; i < 5; i++) {
+            if(current_counters[level] == null)
+                return null;
+        }
         Map<String,Long> hhlist = new HashMap<>();
-        StreamSummary<String> levelSummary = getStreamSummaryForLevel(level);
+        StreamSummary<String> levelSummary = current_counters[level];
         if (level == 4){
             for(Counter<String> entry : levelSummary.topK(epsilon)){
                 if(entry.getCount() >= N * theta){
@@ -101,7 +119,6 @@ public class ReporterBolt implements IRichBolt {
      */
     private static Map<String, Long> sortMap(Map<String, Long> map){
         List list = new LinkedList(map.entrySet());
-        // Defined Custom Comparator here
         Collections.sort(list, new Comparator() {
             public int compare(Object o1, Object o2) {
                 return (((Comparable) ((Map.Entry) (o2)).getValue())
@@ -155,5 +172,38 @@ public class ReporterBolt implements IRichBolt {
             sum += Long.parseLong(res.getString(1));
         }
         return sum;
+    }
+
+    /**
+     * Suggest we calculate total and StreamSummary simultaneously to avoid mismatch of data (line added between calls)
+     * @return
+     * @throws SQLException
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private void getStreamSummaryForLevelAndUpdateTotal() throws SQLException, IOException, ClassNotFoundException {
+        StreamSummary<String>[] allLevels = new StreamSummary[5];
+        long sum = 0;
+        for (int i = 1; i <= 4; i++) {
+            String sqlSelectTotalStreamCommand = "SELECT total, HH FROM Level"+i+" ORDER BY id desc LIMIT 1";
+            ResultSet res = stmt.executeQuery(sqlSelectTotalStreamCommand);
+            res.next();
+            sum += Long.parseLong(res.getString(1));
+            // now, we get the counters back from byteArray:
+
+//            String sqlSelectHHStreamCommand = "SELECT HH FROM Level"+ i +" ORDER BY id desc LIMIT 1";
+//            res = stmt.executeQuery(sqlSelectHHStreamCommand);
+//            res.next();
+
+            String byteArrayAsString = res.getString(2);
+            String[] byteArrayAsStringArray = byteArrayAsString.substring(1,byteArrayAsString.length()-1).split(",");
+            int length = byteArrayAsStringArray.length;
+            byte[] byteArray = new byte[length];
+            for (int j = 0; j < length; j++) {
+                byteArray[j] = Byte.parseByte(byteArrayAsStringArray[j].trim());
+            }
+            current_counters[i] = new StreamSummary<>(byteArray);
+        }
+        N = sum;
     }
 }
